7bb704b90ddd746b2e1eee0693d3f865cb4550f7
kearnz <josephkearney14@gmail.com>
2019-02-06 18:58:46 -0500

    test enhancements

--- tests/test_utils/test_checks.py
+++ tests/test_utils/test_checks.py
@@ -1,13 +1,14 @@
 """
 Pytest for utils.checks
 - check_data_structure requires numpy array, pandas dataframe or list/tuple
-- check_data_structure should return a tuple, with np.array
+- check_data_structure should return with np.array
     - lists, tuples, and dataframes are arrays of dtype object
     - traditional numpy arrays and subclasses of arrays are float
 """
 
 import pytest
-from pandas import Series
+import numpy as np
+import pandas as pd
 from autoimpute.utils.checks import check_data_structure, check_dimensions
 
 @check_data_structure
@@ -26,12 +27,32 @@ def data_structures_not_allowed():
     int_ = 1
     float_ = 1.0
     set_ = set([1, 2, 3])
-    dict_ = dict(a="string", b=1, c=1.0)
-    ser_ = Series({"a":[1, 2, 3, 4]})
+    dict_ = dict(a=str_, b=int_, c=float_)
+    ser_ = pd.Series({"a":[1, 2, 3, 4]})
     return [str_, int_, float_, set_, dict_, ser_]
+
+def data_structure_allowed():
+    """Types that should not throw an error and should return a valid array"""
+    list_ = [[1, 2, 3, 4, np.nan]]
+    tuple_ = ((1, 2, 3, 4, np.nan))
+    array_ = np.array([[1, 2, 3, 4, np.nan]])
+    dataframe_ = pd.DataFrame({"A": list_, "B": list_})
+    return [list_, tuple_, array_, dataframe_]
 
 @pytest.mark.parametrize("ds", data_structures_not_allowed())
 def test_data_structure_not_allowed(ds):
-    """check that both decorators raise a type error for strings"""
+    """check that data structure decorator raise a type error for strings"""
     with pytest.raises(TypeError):
         check_data(ds)
+
+@pytest.mark.parametrize("ds", data_structure_allowed())
+def test_data_structure_allowed(ds):
+    """check that data structure decoractor returns expected types"""
+    arr = check_data(ds)
+    dtype = arr.dtype
+    assert isinstance(arr, np.ndarray)
+    if isinstance(ds, (list, tuple, pd.DataFrame)):
+        assert dtype == np.dtype('object')
+    if isinstance(ds, np.ndarray):
+        assert dtype in (np.dtype('int64'), np.dtype('float64'))
+     
