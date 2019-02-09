89f6c717793a7a60953c293716eec4b5eb1bd1fe
kearnz <josephkearney14@gmail.com>
2019-02-09 10:03:30 -0500

    added missingness check

--- autoimpute/utils/__init__.py
+++ autoimpute/utils/__init__.py
@@ -2,10 +2,12 @@
 
 # import the main functions users should use directly
 from .checks import check_data_structure, check_dimensions
+from .checks import check_missingness
 from .patterns import md_pairs, md_pattern, proportions
 from .patterns import inbound, outbound, influx, outflux, flux
 
 # override from utils import * with main functions
 __all__ = ["check_data_structure", "check_dimensions",
-           "md_pairs", "md_pattern", "proportions",
-           "inbound", "outbound", "influx", "outflux", "flux"]
+           "check_missingness", "md_pairs", "md_pattern",
+           "proportions", "inbound", "outbound",
+           "influx", "outflux", "flux"]
--- autoimpute/utils/checks.py
+++ autoimpute/utils/checks.py
@@ -6,11 +6,13 @@ import pandas as pd
 
 def check_data_structure(func):
     """
-    Decorator that returns a numpy ndarray from an iterator.
+    Check if input value is an allowed iterator.
     Allowed iterators include:
     - native: tuple, list
     - numpy: ndarray or sublcass of ndarray (such as np.mat)
     - pandas: DataFrame (Series not supported yet)
+    Decorator returns a numpy ndarray from an iterator.
+    Least restrictive - simply ensures data type.
     """
     @functools.wraps(func)
     def wrapper(data, *args, **kwargs):
@@ -28,9 +30,11 @@ def check_data_structure(func):
 
 def check_dimensions(func):
     """
-    Check if data structure is 2D
-    If not, throw an error b/c 1D or 3D+ arrays not supported
-    Leverages the _check_data_structure decorator
+    Check if data structure is 2D.
+    If not, throw an error b/c 1D or 3D+ arrays not supported.
+    Leverages the _check_data_structure decorator.
+    Decorator returns data as is or throws an error.
+    Medium restrictive - ensures data type and shape.
     """
     @functools.wraps(func)
     @check_data_structure
@@ -41,4 +45,25 @@ def check_dimensions(func):
         else:
             err = len(data.shape)
             raise TypeError(f"{err}-dimensional arrays not supported")
+    return wrapper
+
+def check_missingness(func):
+    """
+    Check if data structure is all missing or no missing.
+    If all missing, throw error, as can't impute.
+    If no missing, throw error, as nothing to impute.
+    If some missing, simply return func and data.
+    Most restrictive - ensures data type, shape, and missingness.
+    """
+    @functools.wraps(func)
+    @check_dimensions
+    def wrapper(data, *args, **kwargs):
+        """Wrap function to missignness"""
+        missing = pd.isnull(data)
+        if missing.all():
+            raise ValueError("All values missing, need some complete")
+        elif not missing.any():
+            raise ValueError("No missing values, nothing to impute")
+        else:
+            return func(data, *args, **kwargs)
     return wrapper
