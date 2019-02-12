10858388f9e1fe4fbcb3949e0b559e3170a7ce4b
kearnz <josephkearney14@gmail.com>
2019-02-12 12:47:05 -0600

    updated init

--- autoimpute/utils/__init__.py
+++ autoimpute/utils/__init__.py
@@ -3,11 +3,10 @@
 # import the main functions users should use directly
 from .checks import check_data_structure, check_dimensions
 from .checks import check_missingness
-from .patterns import md_pairs, md_pattern, proportions
-from .patterns import inbound, outbound, influx, outflux, flux
+from .patterns import md_pairs, md_pattern, proportions, md_locations
+from .patterns import inbound, outbound, influx, outflux, flux, feature_corr
 
 # override from utils import * with main functions
-__all__ = ["check_data_structure", "check_dimensions",
-           "check_missingness", "md_pairs", "md_pattern",
-           "proportions", "inbound", "outbound",
-           "influx", "outflux", "flux"]
+__all__ = ["check_data_structure", "check_dimensions", "check_missingness",
+           "md_locations", "md_pairs", "md_pattern", "feature_corr",
+           "proportions", "inbound", "outbound", "influx", "outflux", "flux"]
