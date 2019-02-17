469fee8061535b0805834fd495fabc0bad75c877
kearnz <josephkearney14@gmail.com>
2019-02-17 13:22:33 -0500

    init file updates

--- autoimpute/imputations/__init__.py
+++ autoimpute/imputations/__init__.py
@@ -1,1 +1,7 @@
 """Manage imputations lib"""
+
+# import the main functions users should use directly
+from .mis_predictor import MissingnessPredictor
+
+# override from imputations import * with main functions
+__all__ = ["MissingnessPredictor"]
