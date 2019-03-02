a94ef78d44de8693620aa737c2795e5c82c7e371
kearnz <josephkearney14@gmail.com>
2019-03-02 17:37:37 -0500

    updated ts with pylint

--- autoimpute/imputations/ts.py
+++ autoimpute/imputations/ts.py
@@ -1,16 +1,21 @@
 """Time Series Imputation Module"""
 
+import warnings
+import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
-from autoimpute.utils.helpers import _nan_col_dropper
+from sklearn.utils.validation import check_is_fitted
+from autoimpute.utils.helpers import _nan_col_dropper, _mode_output
 from autoimpute.utils.checks import check_missingness
 from autoimpute.utils.checks import _check_strategy, _check_fit_strat
 from autoimpute.imputations.methods import _mean, _median, _mode, _random
-from autoimpute.imputations.methods import _linear, _none
-# pylint:disable=all
+from autoimpute.imputations.methods import _interp, _time, _linear, _none
+# pylint:disable=attribute-defined-outside-init
+# pylint:disable=arguments-differ
 
 class TimeSeriesImputer(BaseEstimator, TransformerMixin):
     """Techniques to impute time series data"""
-    
+
     strategies = {
         "mean": _mean,
         "median": _median,
@@ -25,10 +30,10 @@ class TimeSeriesImputer(BaseEstimator, TransformerMixin):
                  index_column=None, verbose=False, copy=True):
         self.strategy = strategy
         self.fill_value = fill_value
-        self.index_column = index_column 
+        self.index_column = index_column
         self.verbose = verbose
         self.copy = copy
-    
+
     @property
     def strategy(self):
         """return the strategy property"""
@@ -42,22 +47,22 @@ class TimeSeriesImputer(BaseEstimator, TransformerMixin):
 
     def _fit_strategy_validator(self, X):
         """helper method to ensure right number of strategies"""
-        
+
         # first, make sure there is at least one datetime column
         ts = X.select_dtypes(include=[np.datetime64])
         ts_c = len(ts.columns)
         ts_ix = X.index
         if not isinstance(ts_ix, pd.DatetimeIndex):
             if ts_c == 0:
-                err = "DataFrame must have time series index or a time series column"
+                err = "DataFrame must have time series index or column"
                 raise ValueError(err)
-        
+
         # next, strategy check with existing columns passed
         ocols = X.columns.tolist()
         X, self._nc = _nan_col_dropper(X)
         ncols = X.columns.tolist()
         self._strats = _check_fit_strat(self.strategy, self._nc, ocols, ncols)
-        
+
         # finally, reindexing the time column
         if not isinstance(ts_ix, pd.DatetimeIndex):
             fts = ts.columns[0]
@@ -74,15 +79,8 @@ class TimeSeriesImputer(BaseEstimator, TransformerMixin):
                         self._strats.pop(ic)
                         X = X.set_index(ic, drop=True)
                     else:
-                        err = f"{ic} can't be set as DatetimeIndex for DataFrame."
-                        raise KeyError(err)   
-
-        if self.verbose:
-            st = "Strategies used to fit each column:"
-            print(f"{st}\n{'-'*len(st)}")
-            for k, v in self._strats.items():
-                print(f"Column: {k}, Strategy: {v}")
-        
+                        err = f"{ic} can't be set as DatetimeIndex."
+                        raise KeyError(err)
         return X
 
     @check_missingness
@@ -100,8 +98,11 @@ class TimeSeriesImputer(BaseEstimator, TransformerMixin):
             fit_param, fit_name = f(X[col_name])
             self.statistics_[col_name] = {"param":fit_param,
                                           "strategy": fit_name}
+            # print strategies if verbose
             if self.verbose:
-                print(f"{col_name} has {func_name} equal to {fit_param}")
+                st = "Strategies used to fit each column:"
+                print(f"{st}\n{'-'*len(st)}")
+                print(f"Column: {col_name}, Strategy: {fit_name}")
         return self
 
     @check_missingness
