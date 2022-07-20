6c0b26bb39db7f6a9b688cbf6039136d1ae7efe2
kearnz <josephkearney14@gmail.com>
2022-07-19 20:58:17 -0700

    ravel to 1d array from ndarray

--- autoimpute/imputations/series/pmm.py
+++ autoimpute/imputations/series/pmm.py
@@ -174,8 +174,7 @@ class PMMImputer(ISeriesImputer):
         # get the mean and covariance of the multivariate betas
         # betas assumed multivariate normal by linear reg rules
         # sample beta w/ cov structure to create realistic variability
-        extract = tr.stack(sample=["chain", "draw"], inplace=True)
-        alpha_bayes = np.random.choice(extract.alpha)
+        alpha_bayes = np.random.choice(tr.posterior.alpha.ravel())
         beta_means = tr.posterior.beta.mean(0)
         beta_cov = np.cov(tr.posterior.beta.T)
         beta_bayes = np.array(multivariate_normal(beta_means, beta_cov).rvs())
